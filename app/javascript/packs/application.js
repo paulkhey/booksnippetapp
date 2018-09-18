e21a43712f16bc6687c195ded73c948e6f592d94
Paul Kim <pkayokay@gmail.com>
2018-09-17 23:42:36 -0400

    Add Stimulus and render component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
-console.log('Hello World from Webpacker')
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("controllers", true, /.js$/)
+application.load(definitionsFromContext(context))
