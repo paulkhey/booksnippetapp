99f37a11b83623ca5775b8e1a2eecb73a6d3bbb1
Paul Kim <pkayokay@gmail.com>
2018-09-18 23:54:37 -0400

    Refactor - add variable and evalute from controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     my_search = 'Great Gatsby'
+    @book_fields_present = 'book.title && book.image_link && !book.authors.empty?'
     @books = GoogleBooks.search(my_search, {:count => 10})
   end
 end
