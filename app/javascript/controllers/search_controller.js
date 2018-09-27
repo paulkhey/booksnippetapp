90d3856f11aeca8714648fd3cc778fbf8a6c17b0
Paul Kim <pkayokay@gmail.com>
2018-09-27 09:25:44 -0400

    Clean up spacing on search controller

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -21,50 +21,34 @@ export default class extends Controller {
     event.preventDefault()
     this.outputTarget.textContent = `You searched for "${this.input}"`
     var bookResults = []
-
     books.search(this.input, options, function(error, results) {
-        if (!error) {
-
-          var myList = ''
-
-          results.filter(function(result) {
-
-            return !(result.title == undefined || result.authors == undefined || result.thumbnail == undefined)
-
-          }).every(function(result, index) {
-
-              bookResults.push({ title: result.title, author: result.authors[0], thumbnail: result.thumbnail})
-
-              myList += `
-                <li class="search__book">
-                    <img class="search__image" src="${bookResults[index].thumbnail}" alt="${bookResults[index].title} cover">
-                    <div class="search__info">
-                      <p class="search__title">${bookResults[index].title}<p>
-                      <p class="search__author">${bookResults[index].author}</p>
-                    </div>
-                </li>
-              `
-
-            if (index == 9) {
-              return false
-            } else {
-              return true
-            }
-          })
-
-
-          console.log(bookResults.length)
-          myList +=
-
-          document.getElementById('search-results').innerHTML = myList
-
-        } else {
-
-            console.log(error);
-
-            document.getElementById('search-results').innerHTML = '<p style="text-align:center">Sorry, something went wrong.</p>'
-
-        }
+      if (!error) {
+        var myList = ''
+        results.filter(function(result) {
+          return !(result.title == undefined || result.authors == undefined || result.thumbnail == undefined)
+        }).every(function(result, index) {
+          bookResults.push({ title: result.title, author: result.authors[0], thumbnail: result.thumbnail})
+          myList += `
+            <li class="search__book">
+              <img class="search__image" src="${bookResults[index].thumbnail}" alt="${bookResults[index].title} cover">
+              <div class="search__info">
+                <p class="search__title">${bookResults[index].title}<p>
+                <p class="search__author">${bookResults[index].author}</p>
+              </div>
+            </li>
+          `
+          if (index == 9) {
+            return false
+          } else {
+            return true
+          }
+        })
+        myList +=
+        document.getElementById('search-results').innerHTML = myList
+      } else {
+        console.log(error)
+        document.getElementById('search-results').innerHTML = '<p style="text-align:center">Sorry, something went wrong.</p>'
+      }
     });
   }
 }
