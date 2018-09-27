256227aeba421c56d6c9535fb5a2724e6c26c5b2
Paul Kim <pkayokay@gmail.com>
2018-09-27 09:11:56 -0400

    Remove for loop

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,7 +2,7 @@ var books = require('google-books-search')
 
 var options = {
     offset: 0,
-    limit: 15,
+    limit: 10
 }
 
 import { Controller } from "stimulus"
@@ -25,36 +25,38 @@ export default class extends Controller {
 
     books.search(this.input, options, function(error, results) {
         if (!error) {
-          results.filter(function(result) {
-            return !(result.title == undefined && result.authors == undefined && result.thumbnail == undefined)
-          }).map(function(result, index) {
-              bookResults.push({ title: result.title, author: result.authors[0], thumbnail: result.thumbnail})
-          })
 
           var myList = ''
 
-          for ( var i = 0; i < 14; i++) {
-            if (i === 10) {
-                break;
-            }
+          results.filter(function(result) {
 
-            myList += `
-            <li class="search__book">
-                <img class="search__image" src="${bookResults[i].thumbnail}" alt="${bookResults[i].title} cover">
-                <div class="search__info">
-                  <p class="search__title">${bookResults[i].title}<p>
-                  <p class="search__author">${bookResults[i].author}</p>
-                </div>
-              </li>
-            `
-          }
+            return !(result.title == undefined || result.authors == undefined || result.thumbnail == undefined)
 
-          myList+=
+          }).forEach(function(result, index) {
+
+              bookResults.push({ title: result.title, author: result.authors[0], thumbnail: result.thumbnail})
+
+              myList += `
+                <li class="search__book">
+                    <img class="search__image" src="${bookResults[index].thumbnail}" alt="${bookResults[index].title} cover">
+                    <div class="search__info">
+                      <p class="search__title">${bookResults[index].title}<p>
+                      <p class="search__author">${bookResults[index].author}</p>
+                    </div>
+                </li>
+              `
+          })
+
+          myList +=
+
           document.getElementById('search-results').innerHTML = myList
 
         } else {
+
             console.log(error);
+
             document.getElementById('search-results').innerHTML = '<p style="text-align:center">Sorry, something went wrong.</p>'
+
         }
     });
   }
