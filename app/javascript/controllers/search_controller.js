fccaca3d6593343bd33daa8e0427326ee055e826
Paul Kim <pkayokay@gmail.com>
2018-09-30 21:50:53 -0400

    Remove old comments

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -52,16 +52,6 @@ export default class extends Controller {
             return !(result.title == undefined || result.authors == undefined || result.thumbnail == undefined)
           }).every(function(result, index) {
             bookResults.push({ title: truncate(result.title), author: result.authors[0], thumbnail: prependURL(result.thumbnail)})
-            // myList += `
-            //   <li class="search__book">
-            //     <img class="search__image" src="${bookResults[index].thumbnail}" alt="${bookResults[index].title} cover">
-            //     <div class="search__info">
-            //       <p class="search__title">${bookResults[index].title}<p>
-            //       <p class="search__author">${bookResults[index].author}</p>
-            //     </div>https://www.npmjs.com/package/paginate-array?activeTab=dependencies
-            //   </li>
-            // `
-
             if (index == 11) {
               return false
             } else {
@@ -83,11 +73,6 @@ export default class extends Controller {
               $('#search-results').append(`<div class="search__sets page-${index+1}" >Page ${index + 1}</div>`)
 
               for (var i = 0; i <= sets.length - 1; i ++) {
-                // console.log('')
-                // console.log(sets[i].title)
-                // console.log(sets[i].author)
-                // console.log(sets[i].thumbnail)
-                console.log(i)
                 $('.search__sets:nth-of-type(' + index).append(`
                   <li>
                     <div class="search__book">
@@ -98,6 +83,7 @@ export default class extends Controller {
                       </div>
                     </div>
                   </li>`)
+
               }
               console.log('')
             })
@@ -108,12 +94,6 @@ export default class extends Controller {
           console.log(error)
         }
       })
-
-
-
-    // var myList = ''
-    // document.getElementById('search-results').innerHTML = myList
-
     } else {
       $('#search-results').html('')
     }
