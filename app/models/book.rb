b0b4f8c052250422ff9d08a6418bb6574e05321d
Paul Kim <pkayokay@gmail.com>
2018-10-05 23:55:04 -0400

    Comment out validation to test pg

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   validates :title, presence: true, :uniqueness => true
   validates :author, presence: true
-  validates :cover, presence: true
-  validates :link, presence: true
+  # validates :cover, presence: true
+  # validates :link, presence: true
 end
