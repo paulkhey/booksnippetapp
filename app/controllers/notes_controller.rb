b2eb95672f7c8e495f1f48f0333ae08cc17a6956
Paul Kim <pkayokay@gmail.com>
2018-10-13 19:02:41 -0400

    Reformat note controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,16 +13,19 @@ class NotesController < ApplicationController
     @book = Book.find(params[:book_id])
   end
   
+  def create
+    @book = Book.find(params[:book_id])
+    @note = @book.notes.create(note_params)
+    redirect_to book_path(@book)
+  end
+  
   def update 
     @note = Note.find(params[:id])
     @note.update(note_params)
     redirect_to book_note_path(@note)
   end
   
-  def create
-    @book = Book.find(params[:book_id])
-    @note = @book.notes.create(note_params)
-    redirect_to book_path(@book)
+  def destroy
   end
  
   private
